8df6cdd1ed6f22e1635da2b4cc212a38bcf378ee
NeliielDW <n.miannay@gmail.com>
2023-09-25 15:43:48 +0200

    Password - JS

---
+++ javascript/pass-show-hide.js
@@ -0,0 +1,14 @@
+/* Mise en place du mot de passe visible ou non */
+
+const passwordField = document.querySelector(".form input[type='password']");
+const toggleBtn = document.querySelector(".form .field i");
+
+toggleBtn.onclick = ()=>{
+    if (passwordField.type == "password"){
+        passwordField.type = "text";
+        toggleBtn.classList.add("active");
+    } else {
+        passwordField.type = "password";
+        toggleBtn.classList.remove("active");
+    }
+}
