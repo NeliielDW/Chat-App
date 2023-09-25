ad9b03ffddf806ed37978444bbf07e9b1f222340
NeliielDW <n.miannay@gmail.com>
2023-09-25 16:01:34 +0200

    Users - JS

---
+++ javascript/users.js
@@ -0,0 +1,10 @@
+/* Rechercher un utilisateur */
+const searchBar = document.querySelector(".users .search input");
+const searchBtn = document.querySelector(".users .search button");
+
+searchBtn.onclick = ()=>{
+    searchBar.classList.toggle("active");
+    searchBar.focus();
+    searchBtn.classList.toggle("active");
+}
+
